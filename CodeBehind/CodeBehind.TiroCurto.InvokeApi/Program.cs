1153e857f3ac4e5d8b3453485143023da3ea12d4
skimoh <rodolfocomp@gmail.com>
2020-11-25 18:58:04 -0300

    Inclusao de Projeto RestSharp

---
+++ CodeBehind/CodeBehind.TiroCurto.InvokeApi/Program.cs
@@ -0,0 +1,64 @@
+﻿//***CODE BEHIND - BY RODOLFO.FONSECA***//
+using RestSharp;
+using System;
+
+namespace CodeBehind.TiroCurto.InvokeApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InvocarGet();
+            InvocarPost();
+        }
+
+        private static void InvocarPost()
+        {
+            var client = new RestClient($"https://run.mocky.io/v3/73f60d83-f20d-4724-8d54-f98be085dd1d");
+
+            RestRequest request = new RestRequest("", Method.POST);
+            request.AddJsonBody(new { Id = 5 });
+            var response = client.Execute(request);
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(response.Content);
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadKey();
+        }
+
+        private static void InvocarGet()
+        {
+            string cep = "37130031";
+
+            var client = new RestClient($"https://viacep.com.br/ws/{cep}/json/");
+            RestRequest request = new RestRequest("", Method.GET);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                Console.WriteLine(response.Content);
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class correio
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+        public string ddd { get; set; }
+        public string siafi { get; set; }
+    }
+}
