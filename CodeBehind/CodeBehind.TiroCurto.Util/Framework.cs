5bd45552d3c5edda02d3b5690a8fa5660e828aae
skimoh <rodolfocomp@gmail.com>
2021-03-30 11:30:01 -0300

    Inclusao do projeto utilitario

---
+++ CodeBehind/CodeBehind.TiroCurto.Util/Framework.cs
@@ -0,0 +1,193 @@
+﻿//***CODE BEHIND - BY RODOLFO.FONSECA***//
+
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CodeBehind.TiroCurto.Util
+{
+    public static class Framework
+    {
+        private static string regexBCGlobal = @"^(6541|6556)[0-9]{12}$";
+        private static string regexCarteBlancheCard = @"^389[0-9]{11}$";
+        private static string regexDiscoverCard = @"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$";
+        private static string regexInstaPaymentCard = @"^63[7-9][0-9]{13}$";
+        private static string regexJCBCard = @"^(?=2131|1800|35\d{3})\d{11}$";
+        private static string regexKoreanLocalCard = @"^9[0-9]{15}$";
+        private static string regexLaserCard = @"^(6304|6706|6709|6771)[0-9]{12,15}$";
+        private static string regexSoloCard = @"^(6334|6767)[0-9]{12}|(6334|6767)[0-9]{14}|(6334|6767)[0-9]{15}$";
+        private static string regexSwitchCard = @"^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}$";
+        private static string regexUnionPayCard = @"^(62[0-9]{14,17})$";
+
+        private static string regexAmexCard = @"^3[47][0-9]{13}$";
+        private static string regexDinersClubCard = @"^3(?:0[0-5]|[68][0-9])[0-9]{11}$";
+        private static string regexMaestroCard = @"^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}$";
+        private static string regexMasterCard = @"^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$";
+        private static string regexVisaCard = @"^4[0-9]{12}(?:[0-9]{3})?$";
+        private static string regexVisaMasterCard = @"^(?=4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$";
+        private static string regexELO = @"^(5067|40117[8-9]|431274|438935|451416|457393|45763[1-2]|506(699|7[0-6][0-9]|77[0-8])|509\d{3}|504175|627780|636297|636368|65003[1-3]|6500(3[5-9]|4[0-9]|5[0-1])|6504(0[5-9]|[1-3][0-9])|650(4[8-9][0-9]|5[0-2][0-9]|53[0-8])|6505(4[1-9]|[5-8][0-9]|9[0-8])|6507(0[0-9]|1[0-8])|65072[0-7]|6509(0[1-9]|1[0-9]|20)|6516(5[2-9]|[6-7][0-9])|6550([0-1][0-9]|2[1-9]|[3-4][0-9]|5[0-8]))\d{0,12}";
+
+        public static bool IsValidCartao(string bandeira, string nrCartao)
+        {
+            var ehValido = false;
+            Regex rg;
+
+            switch (bandeira)
+            {
+                case "VISA":
+                    rg = new Regex(regexVisaCard);
+                    ehValido = rg.IsMatch(nrCartao);
+                    break;
+
+                case "MASTER":
+                    rg = new Regex(regexMasterCard);
+                    ehValido = rg.IsMatch(nrCartao);
+                    break;
+                    //COMPLETAR
+            }
+
+            return ehValido;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsCpf(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+
+            for (int j = 0; j < 10; j++)
+                if (j.ToString().PadLeft(11, char.Parse(j.ToString())) == cpf)
+                    return false;
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+
+        public static bool IsCnpj(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cnpj = cnpj.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+
+            string tempCnpj = cnpj.Substring(0, 12);
+            int soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+
+            int resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cnpj.EndsWith(digito);
+        }
+
+        public static bool IsCEP(string cep)
+        {
+
+            Regex Rgx = new Regex(@"^\d{5}-\d{3}$");
+
+            if (!Rgx.IsMatch(cep))
+                return false;
+            else
+                return true;
+        }
+
+        public static bool IsCelular(string numero)
+        {
+            var numeroLimpo = numero.Replace("-", "").Replace("(", "").Replace(")", "");
+            return numeroLimpo.Length == 11;
+        }
+    }
+}
