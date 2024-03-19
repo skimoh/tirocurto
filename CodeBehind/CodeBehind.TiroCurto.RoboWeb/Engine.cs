071fc07b0cb7131492a00792ccbbb55b743d168f
Rodolfo Fonseca <rodolfocomp@gmail.com>
2024-03-19 16:07:07 -0300

    Robo Web Com Selenium

---
+++ CodeBehind/CodeBehind.TiroCurto.RoboWeb/Engine.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CodeBehind.TiroCurto.RoboWeb
+{
+    public class Engine
+    {
+        private IWebDriver _driver;
+        public Engine()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Abrir()
+        {
+            //acessando o site
+            _driver.Navigate().GoToUrl("https://www.popsistemas.com.br/");
+
+            //acionando o botao
+            _driver.FindElement(By.XPath("//*[@id=\'navbarResponsive\']/ul/li[4]/a")).Click();
+
+            var texto = _driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/p[1]/a")).Text;
+
+            Console.Write($"Telefone de contato {texto}");
+
+            //preenchendo formulario
+            //_driver.FindElement(By.XPath("")).SendKeys("Fulano");
+
+
+            //TODO: captcha / teclado virtual / tempo de carregamento
+
+            //submetendo
+            //_driver.FindElement(By.XPath("")).Submit();
+
+            //_driver.Quit();
+        }
+    }
+}
---
+++ CodeBehind/CodeBehind.TiroCurto.RoboWeb/Program.cs
@@ -0,0 +1,4 @@
+﻿using CodeBehind.TiroCurto.RoboWeb;
+
+var navegador = new Engine();
+navegador.Abrir();
